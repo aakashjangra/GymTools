640d46c1eaba81f7ca9f2c450aed6ea1be648f0c
sky <aakashjangra2002@gmail.com>
2023-07-04 10:46:28 +0530

    Homepage design and product addition

--- app/page.js
+++ app/page.js
@@ -62,52 +62,81 @@ export default function Home() {
                   Crafted with care. Built for results.
               </h3>
               <p className={styles.description}>
-                  Equipment that's built to last, and designed to help you
-                  reach your goals
+                  Equipment that's built to last, and designed to help you reach
+                  your goals
               </p>
-                {
-                    products?.length > 0 ?
-                    <section className={styles.products}>
-                       { function renderEightProducts() {
-                           let renderedProducts = [];
-                           for(let i = 0; i<8; i++){
-                               const product = products[i];
-                               renderedProducts.push(
-                                   <div
-                                       className={`${styles.card} ${styles.product}`}
-                                       key={product.id}
-                                   >
-                                       {product.imageUrls?.length > 0 ? (
-                                           <img
-                                               className={
-                                                   styles.productPreviewImg
-                                               }
-                                               src={product.imageUrls[0]}
-                                               alt="Product Preview Image"
-                                           />
-                                       ) : (
-                                           <></>
-                                       )}
-                                       <h4 className={styles.productName}>
-                                           {product.title}
-                                       </h4>
-                                       <div className={styles.pricing}>
-                                           <p className={`${styles.price} ${styles.strikeThrough}`}>
-                                               ₹ {product.price} INR
-                                           </p>
-                                           <p className={styles.salePrice}>
-                                               ₹ {product.salePrice} INR
-                                           </p>
-                                       </div>
-                                   </div>
-                               );
-                            }
-                            return renderedProducts;
-                        } ()
-                        }
-                    </section>
-                    : <p>No products right now.</p>
-                }
+              {products?.length > 0 ? (
+                  <section className={styles.products}>
+                      {(function renderEightProducts() {
+                          let renderedProducts = [];
+                          for (let i = 0; i < 8; i++) {
+                              const product = products[i];
+                              renderedProducts.push(
+                                  <div
+                                      className={`${styles.card} ${styles.product}`}
+                                      key={product.id}
+                                  >
+                                      <div className={styles.productImages}>
+                                          {product.imageUrls?.length > 0 ? (
+                                              <>
+                                                  <img
+                                                      className={`${styles.productPreviewImg} ${styles.imageNormal}`}
+                                                      src={product.imageUrls[0]}
+                                                      alt="Product Preview Image"
+                                                  />
+                                                  <img
+                                                      className={`${styles.productPreviewImg} ${styles.imageHover}`}
+                                                      src={product.imageUrls?.length > 1? product.imageUrls[1]: product.imageUrls[0]}
+                                                      alt="Product Preview Image"
+                                                  />
+                                              </>
+                                          ) : (
+                                              <></>
+                                          )}
+                                      </div>
+                                      <h4 className={styles.productName}>
+                                          {product.title}
+                                      </h4>
+                                      <div className={styles.pricing}>
+                                          <p
+                                              className={`${styles.price} ${styles.strikeThrough}`}
+                                          >
+                                              ₹ {product.price} INR
+                                          </p>
+                                          <p className={styles.salePrice}>
+                                              ₹ {product.salePrice} INR
+                                          </p>
+                                      </div>
+                                  </div>
+                              );
+                          }
+                          return renderedProducts;
+                      })()}
+                  </section>
+              ) : (
+                  <p>No products right now.</p>
+              )}
+          </section>
+          <section className={styles.reviews}>
+              <section className={styles.review}>
+                  <h5 className={styles.name}>Sophie</h5>
+                  <p className={styles.reviewContent}>
+                      "I've been looking for a new gym equipment store for a
+                      while now, and I'm so glad I found GymTools. They have a
+                      great selection of equipment, and the prices are very
+                      reasonable. But what really sets them apart is their
+                      customer service. "
+                  </p>
+              </section>
+              <section className={styles.review}>
+                  <h5 className={styles.name}>Jack</h5>
+                  <p className={styles.reviewContent}>
+                      "The delivery and setup service was very convenient. They
+                      delivered the equipment to my home and set it up for me.
+                      This was a huge help, as I didn't have to worry about
+                      doing it myself."
+                  </p>
+              </section>
           </section>
       </main>
   );
